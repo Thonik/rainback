d68fa22f32fbbd6cbf6e8479af296c22b2fab3be
Aaron Faanes <dafrito@gmail.com>
2013-04-19 06:11:48 -0500

    Removed debugging methods

--- scripts/foo.lua
+++ scripts/foo.lua
@@ -7,19 +7,3 @@ require "fritomod/Events";
 -- in a better way.
 gfx = Rainback.Graphics:New();
 Events.RENDER(gfx, "Render");
-
-Events.MOUSEPRESS(function(button, x, y)
-    print("Mouse press: " .. button .. " at " .. x .. ", " .. y);
-end);
-
-Events.MOUSERELEASE(function(button, x, y)
-    print("Mouse release: " .. button .. " at " .. x .. ", " .. y);
-end);
-
-Events.MOUSEMOVE(function(x, y)
-    --print("Mouse move: " .. x .. ", " .. y);
-end);
-
-Events.MOUSEWHEEL(function(delta)
-    --print("Wheel delta is: " .. delta)
-end);
