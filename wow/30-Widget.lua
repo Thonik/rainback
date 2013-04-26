212a5d3cc081bfe8dde7c0d9aab1614b2648ee4f
Aaron Faanes <dafrito@gmail.com>
2013-04-26 04:19:27 -0500

    Expose Widget frame type, for Qt

---
+++ wow/30-Widget.lua
@@ -0,0 +1,40 @@
+require "wow/Frame";
+require "rainback/AnchoredBound";
+require "rainback/wow-conversion";
+
+local Widget = OOP.Class(WoW.Frame);
+WoW.Widget = Widget;
+WoW.RegisterFrameType("Widget", Widget);
+
+function Widget:GetWidget()
+    return self.widget;
+end;
+
+function Widget:SetWidget(widget)
+    self.widget = widget;
+end;
+
+local Delegate = OOP.Class();
+
+function Delegate:Constructor(frame)
+    self.frame = frame;
+end;
+
+function Delegate:Draw(painter)
+    local frame = self.frame;
+    local bound = frame:GetDelegate("layout"):GetBounds();
+    if type(bound) ~= "table" or not bound:HasBounds() then
+        return;
+    end;
+    local widget = frame:GetWidget();
+    if not widget then
+        return;
+    end;
+    widget:setGeometry(bound:GetBounds());
+end;
+
+function Delegate:ToString()
+    return "[Rainback Widget-Draw Delegate]";
+end;
+
+WoW.SetFrameDelegate("Widget", "draw", Delegate, "New");
