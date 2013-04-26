64976c0656a15428531b0fa391110efcd3083978
Aaron Faanes <dafrito@gmail.com>
2013-04-25 21:19:49 -0500

    Support userdata for QWidget

--- src/Rainback.cpp
+++ src/Rainback.cpp
@@ -7,9 +7,48 @@
 #include <lua-cxx/LuaValue.hpp>
 #include <lua-cxx/loaders.hpp>
 #include <lua-cxx/userdata.hpp>
+#include <lua-cxx/QObjectObserver.hpp>
 
 #include "LuaPainter.hpp"
 #include "LuaFont.hpp"
+
+namespace lua {
+
+template<>
+struct UserdataType<QWidget>
+{
+    constexpr static const char* name = "QWidget";
+
+    static void initialize(LuaStack& stack, QWidget& widget)
+    {
+        auto userdata = stack.save();
+        lua::push(stack, userdata);
+        lua::userdata::qobject(stack, widget);
+
+        lua::push(stack, lua::value::table);
+        auto methods = stack.save();
+
+        methods["resize"] = std::function<void(QWidget&, const int, const int)>(
+            [](QWidget& widget, const int w, const int h) {
+                widget.resize(w, h);
+            }
+        );
+
+        auto worker = stack.lua()(""
+            "return function(userdata, methods)\n"
+            "    local mt = getmetatable(userdata);\n"
+            ""
+            "    local defaultIndex = mt.__index;\n"
+            "    function mt.__index(self, key)\n"
+            "        return methods[key] or defaultIndex(self, key);"
+            "    end;\n"
+            "end;\n"
+        );
+        worker(userdata, methods);
+    }
+};
+
+} // namespace lua
 
 std::shared_ptr<LuaFont> newFont(LuaStack& stack)
 {
