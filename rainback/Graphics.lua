cdd19b5cc2be2db129406e66335a3534ad2e34e9
Aaron Faanes <dafrito@gmail.com>
2013-05-02 02:44:30 -0500

    Output useful stats on framerate

--- rainback/Graphics.lua
+++ rainback/Graphics.lua
@@ -36,6 +36,24 @@ function Rainback.Render(painter)
     painter:setPenColor(0, 0, 0, 255);
     painter:drawText("Rainback v1.0");
 
-    trace("Relative render time: " .. ((Rainback.GetTime() - start) / (1000 / 60)));
+    local elapsed = Rainback.GetTime() - start;
+    if elapsed > 0 then
+        local fps = 1000 / elapsed;
+
+        local bar = "";
+        local SCALE = 2;
+        local GOAL = 1000 / 60;
+        for i=1, math.floor(elapsed / SCALE) do
+            if i * SCALE > GOAL then
+                bar = bar .. "!";
+            else
+                bar = bar .. ".";
+            end;
+        end;
+
+        printf("%3dms elapsed, %2dfps %s", elapsed, fps, bar);
+    else
+        printf("  0ms elapsed");
+    end;
     trace("FRAME complete");
 end;
