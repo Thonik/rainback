851dc1e610d4e399c8ab490f4a3dbce74283053f
Aaron Faanes <dafrito@gmail.com>
2013-05-04 07:23:27 -0500

    Provide the ability to autosave

--- wow/50-Persistence.lua
+++ wow/50-Persistence.lua
@@ -10,7 +10,70 @@ function Rainback.LoadPersistence(persistenceFile)
     end;
     Persistence.Load(persisted);
 
+    local index;
+    local indexFile = persistenceFile .. ".index";
+
+    local MAX_ENTRIES = 9;
+    local AUTOSAVE_INTERVAL = "3m";
+
+    local function GetIndex()
+        if index ~= nil then
+            return index;
+        end;
+        if not Rainback.FileExists(indexFile) then
+            index = 1;
+            return index;
+        end;
+        local indexData = Rainback.ReadFile(indexFile);
+        index = tonumber(indexData);
+        if index == nil then
+            printf("Failed to read index %q", indexData);
+            index = 1;
+        end;
+        return index;
+    end;
+
+    local function WriteIndex()
+        Rainback.WriteFile(indexFile, index);
+    end;
+
+    local function BumpIndex()
+        index = index + 1;
+        if index > MAX_ENTRIES then
+            index = 1;
+        end;
+        WriteIndex();
+    end;
+
+    function Rainback.Autosave()
+        Persistence.Save();
+        local persistedData = Serializers.WriteSource(persisted);
+
+        local oldFilename = persistenceFile .. "." .. GetIndex();
+        local savedData;
+        if Rainback.FileExists(oldFilename) then
+            savedData = Rainback.ReadFile(oldFilename);
+            if savedData == persistedData then
+                -- Nothing changed, so nothing to save.
+                return;
+            end;
+            BumpIndex();
+        else
+            WriteIndex();
+        end;
+
+        print("Autosaving");
+        Rainback.WriteFile(persistenceFile .. "." .. GetIndex(), persistedData);
+    end;
+    local remover = Timing.Every(AUTOSAVE_INTERVAL, function()
+        local success, msg = xpcall(Rainback.Autosave, debug.traceback);
+        if not success then
+            print("WARNING: Autosave failed: ".. msg);
+        end;
+    end);
+
     Rainback.OnNamedEvent("CLOSE", function()
+        remover();
         Persistence.Save();
         Rainback.WriteFile(persistenceFile, Serializers.WriteSource(persisted));
     end);
